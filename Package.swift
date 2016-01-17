08cc86444b73160ea79a51425e8ec005ab3198b3
Adam Sharp <adsharp@me.com>
2016-01-18 08:42:40 +1100

    Add libc wrapper module

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Maelstrom",
   targets: [
+    Target(name: "Maelstrom", dependencies: [.Target(name: "libc")]),
     Target(name: "rename", dependencies: [.Target(name: "Maelstrom")]),
   ],
   dependencies: [
