60aa3b0d2b61c0a4908c28b62420e671c8a9b347
Adam Sharp <adsharp@me.com>
2016-01-20 08:35:50 +1100

    Add missing implicit dependency on libc

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
   name: "Maelstrom",
   targets: [
     Target(name: "Maelstrom", dependencies: [.Target(name: "libc")]),
+    Target(name: "util", dependencies: [.Target(name: "libc")]),
     Target(name: "read", dependencies: [
       .Target(name: "util"),
       .Target(name: "Maelstrom")]),
